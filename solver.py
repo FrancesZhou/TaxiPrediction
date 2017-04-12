7ca4ec4791f1a6e966cd79d0b256927f517b6c0a
Frances <zhouxian@sjtu.edu.cn>
2017-04-12 21:07:26 +0800

    test bug

--- solver.py
+++ solver.py
@@ -190,12 +190,13 @@ class ModelSolver(object):
 				#start_t = time.time()
 				t_loss = 0
 				i = pre_index
-				while i<pre_index+3:
+				while i<pre_index+21:
 				#while i<len(seq)-n:
 					# seq_i : pre_index+n
 					if i%100 == 0:
 						print("test_1_to_n at i = "+str(i))
-					seq_i = seq[i-pre_index: i+n]
+					#seq_i = seq[i-pre_index: i+n]
+					seq_i = seq
 					#print(seq_i.shape)
 					time_i = timestamps[i-pre_index: i+n]
 					#loss_i = 0
@@ -204,11 +205,12 @@ class ModelSolver(object):
 						#					batch_size=1, close=close, period=period, trend=trend)
 						x = []
 						for d in range(len(depends)):
-							x_d = np.transpose(np.vstack(np.transpose(seq_i[n_i+pre_index-np.array(depends[d]), :, :, :],[0,3,1,2])), [1,2,0])
+							#x_d = np.transpose(np.vstack(np.transpose(seq_i[n_i+pre_index-np.array(depends[d]), :, :, :],[0,3,1,2])), [1,2,0])
+							x_d = np.transpose(np.vstack(np.transpose(seq_i[n_i+i-np.array(depends[d]), :, :, :],[0,3,1,2])), [1,2,0])
 							#print(x_d.shape)
 							x_d = np.expand_dims(x_d, axis=0)
 							x.append(x_d)
-						ext_i = time.strptime(time_i[n_i+pre_index][:8], '%Y%m%d').tm_wday
+						ext_i = time.strptime(time_i[n_i+i][:8], '%Y%m%d').tm_wday
 						v = [0 for _ in range(7)]
 						v[ext_i] = 1
 						if ext_i >= 5:
@@ -228,12 +230,12 @@ class ModelSolver(object):
 						y_p, _ = sess.run([y_, loss], feed_dict=feed_dict)
 						l = np.sum(np.square(y_p-y))
 						print(l)
-						seq_i[n_i+pre_index] = y_p
+						seq_i[n_i+i] = y_p
 						t_loss += l
 						#loss_i += l
 					#y_pred_all.append(seq_i[pre_index:])
 					#t_loss += loss_i
-					i += 1
+					i += 10
 				row, col, flow = np.array(seq).shape[1:]
 				print(row,col,flow)
 				test_count = (len(seq)-pre_index-n)*n*(row*col*flow)
