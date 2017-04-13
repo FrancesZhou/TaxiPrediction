3e3cc0d0c00c87855042098ca03a657e0e8d28f1
Frances <zhouxian@sjtu.edu.cn>
2017-04-13 16:08:18 +0800

    arma_10_taxi.py

--- arma_10_taxi.py
+++ arma_10_taxi.py
@@ -24,24 +24,21 @@ from preprocessing import *
 input_steps = 10
 output_steps = 10
 run_times = 480
-#pre_process = MinMaxNormalization01()
 print('load train, validate, test data...')
 split = [43824, 8760, 8760]
 data, train_data, val_data, test_data = load_data(filename=['data/p_map.mat', 'data/d_map.mat'], split=split)
 # data: [num, row, col, channel]
 print('preprocess train data...')
-#pre_process.fit(train_data)
 all_timestamps_string = gen_timestamps(['2009','2010','2011','2012','2013','2014','2015'], gen_timestamps_for_year=gen_timestamps_for_year_ymdh)
 all_timestamps_struct = [time.strptime(t, '%Y%m%d%H') for t in all_timestamps_string]
 timestamps = [datetime.fromtimestamp(mktime(t)) for t in all_timestamps_struct]
 # data: [num, row, col, channel]
-print('preprocess train data...')
-#data = pre_process.transform(data)
+print('preprocess and get test data...')
 data = np.reshape(data, (data.shape[0], -1))
-val_data = data[split[0]:split[0]+split[1]]
+#val_data = data[split[0]:split[0]+split[1]]
 test_data = data[split[0]+split[1]:]
 data = np.transpose(data)
-val_data = np.transpose(val_data)
+#val_data = np.transpose(val_data)
 test_data = np.transpose(test_data)
 # data: [64*64*2, num]
 #train_data = data[:][:split[0]]
@@ -77,6 +74,7 @@ test_data = np.transpose(test_data)
 print('======================= ARMA for test ===============================')
 loss = 0
 for r in range(run_times):
+    print('run '+str(r))
     i = np.random.randint(data.shape[0])
     j = np.random.randint(test_data.shape[-1]-output_steps)
     train_df = pd.DataFrame(data[i][j:split[0]+split[1]+j])
@@ -85,7 +83,7 @@ for r in range(run_times):
     pre, _, _ = results.forecast(output_steps)
     test_real = test_data[i][j:j+output_steps]
     loss += np.sum(np.square(pre - test_real))
-print('================ calculate rmse for validate and test data ============')
+print('================ calculate rmse for test data ============')
 #n_rmse_val = np.sqrt(np.sum(np.square(val_predict - val_real))*1.0/np.prod(val_real.shape))
 #n_rmse_test = np.sqrt(np.sum(np.square(test_predict - test_real))*1.0/np.prod(test_real.shape))
 #rmse_val = pre_process.real_loss(n_rmse_val)
@@ -93,6 +91,7 @@ print('================ calculate rmse for validate and test data ============')
 #print('val loss is ' + str(n_rmse_val) + ' , ' + str(rmse_val))
 #print('test loss is ' + str(n_rmse_test) + ' , ' + str(rmse_test))
 #print('val loss is ' + str(n_rmse_val))
+print('run times: '+str(run_times))
 rmse = np.sqrt(loss/(run_times*output_steps))
-print('test loss is ' + str(n_rmse_test))
+print('test loss is ' + str(rmse))
 
