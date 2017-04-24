26e349468380b6dd05afa5bbfdc16d22d4f21d5c
Frances <zhouxian@sjtu.edu.cn>
2017-04-24 17:39:45 +0800

    add citybike

---
+++ citybike-NYC/arma_10_citybike.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+from scipy import stats
+from time import mktime
+from datetime import datetime
+import pandas as pd
+
+import statsmodels.api as sm
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.vector_ar.var_model import VAR
+import sys
+# for mac debug
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/model/')
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/util/')
+# for server running
+sys.path.append('/home/zx/TaxiPrediction/model/')
+sys.path.append('../util/')
+sys.path.append('../data/')
+from utils import *
+from preprocessing import *
+
+input_steps = 10
+output_steps = 10
+run_times = 1000
+print('load train, validate, test data...')
+split = [17520, 4416, 4368]
+data, train_data, val_data, test_data = load_npy_data(filename=['../data/citybike/p_map.npy', '../data/citybike/d_map.npy'], split=split)
+# data: [num, row, col, channel]
+print('preprocess train data...')
+all_timestamps_string = gen_timestamps(['2013','2014','2015','2016'], gen_timestamps_for_year=gen_timestamps_for_year_ymdh)
+all_timestamps_string = all_timestamps_string[4344:-4416]
+all_timestamps_struct = [time.strptime(t, '%Y%m%d%H') for t in all_timestamps_string]
+timestamps = [datetime.fromtimestamp(mktime(t)) for t in all_timestamps_struct]
+# data: [num, row, col, channel]
+print('preprocess and get test data...')
+data = np.reshape(data, (data.shape[0], -1))
+#val_data = data[split[0]:split[0]+split[1]]
+test_data = data[split[0]+split[1]:]
+data = np.transpose(data)
+#val_data = np.transpose(val_data)
+test_data = np.transpose(test_data)
+# data: [64*64*2, num]
+#train_data = data[:][:split[0]]
+
+#train_timestamps = timestamps[:split[0]]
+# validate and test data
+# val_real = np.zeros((data.shape[0], val_data.shape[-1]-output_steps, output_steps))
+# val_predict = np.zeros(val_real.shape)
+# test_real = np.zeros((data.shape[0], test_data.shape[-1]-output_steps, output_steps))
+# test_predict = np.zeros(test_real.shape)
+# ARMA for validate data
+# print('======================== ARMA for validate ==========================')
+# for i in range(data.shape[0]):
+#     print('validate, i = '+str(i))
+#     for j in range(val_data.shape[-1]-output_steps):
+#         train_df = pd.DataFrame(data[i][j:split[0]+j])
+#         train_df.index = pd.DatetimeIndex(timestamps[j:split[0]+j])
+#         results = ARMA(train_df, order=(2,2)).fit(trend='nc', disp=-1)
+#         pre, _, _ = results.forecast(output_steps)
+#         val_real[i][j] = val_data[i][j:j+output_steps]
+#         val_predict[i][j] = pre
+# ARMA for test data
+# print('======================= ARMA for test ===============================')
+# for i in range(data.shape[0]):
+#     print('test, i = '+str(i))
+#     for j in range(test_data.shape[-1]-output_steps):
+#         train_df = pd.DataFrame(data[i][j:split[0]+split[1]+j])
+#         train_df.index = pd.DatetimeIndex(timestamps[j:split[0]+split[1]+j])
+#         results = ARMA(train_df, order=(2,2)).fit(trend='nc', disp=-1)
+#         pre, _, _ = results.forecast(output_steps)
+#         test_real[i][j] = test_data[i][j:j+output_steps]
+#         test_predict[i][j] = pre
+print('======================= ARMA for test ===============================')
+loss = 0
+error_count = 0
+index_all = np.zeros([run_times, 2])
+error_index = np.zeros(run_times)
+test_target = np.zeros([run_times, output_steps])
+test_prediction = np.zeros([run_times, output_steps])
+for r in range(run_times):
+    print('run '+str(r))
+    i = np.random.randint(data.shape[0])
+    j = np.random.randint(test_data.shape[-1]-output_steps)
+    train_df = pd.DataFrame(data[i][j:split[0]+split[1]+j])
+    train_df.index = pd.DatetimeIndex(timestamps[j:split[0]+split[1]+j])
+    try:
+        results = ARMA(train_df, order=(2,2)).fit(trend='nc', disp=-1)
+    except:
+        error_index[error_count] = r
+        error_count += 1
+        continue
+    pre, _, _ = results.forecast(output_steps)
+    test_real = test_data[i][j:j+output_steps]
+    index_all[r] = [i,j]
+    test_target[r] = test_real
+    test_prediction[r] = pre
+    loss += np.sum(np.square(pre - test_real))
+print('================ calculate rmse for test data ============')
+#n_rmse_val = np.sqrt(np.sum(np.square(val_predict - val_real))*1.0/np.prod(val_real.shape))
+#n_rmse_test = np.sqrt(np.sum(np.square(test_predict - test_real))*1.0/np.prod(test_real.shape))
+#rmse_val = pre_process.real_loss(n_rmse_val)
+#rmse_test = pre_process.real_loss(n_rmse_test)
+#print('val loss is ' + str(n_rmse_val) + ' , ' + str(rmse_val))
+#print('test loss is ' + str(n_rmse_test) + ' , ' + str(rmse_test))
+#print('val loss is ' + str(n_rmse_val))
+print('run times: '+str(run_times))
+print('error count: '+str(error_count))
+rmse = np.sqrt(loss/((run_times-error_count)*output_steps))
+print('test loss is ' + str(rmse))
+np.save('../citybike-results/results/ARMA/test_target.npy', test_target)
+np.save('../citybike-results/results/ARMA/test_prediction.npy', test_prediction)
+np.save('../citybike-results/results/ARMA/index_all.npy', index_all)
+np.save('../citybike-results/results/ARMA/error_index.npy', error_index)
+
---
+++ citybike-NYC/var_10_citybike.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+from scipy import stats
+from time import mktime
+from datetime import datetime
+import pandas as pd
+
+import statsmodels.api as sm
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.vector_ar.var_model import VAR
+import sys
+# for mac debug
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/model/')
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/util/')
+# for server running
+sys.path.append('/home/zx/TaxiPrediction/model/')
+sys.path.append('../util/')
+sys.path.append('../data/')
+from utils import *
+from preprocessing import *
+
+input_steps = 10
+output_steps = 10
+pre_process = MinMaxNormalization01()
+print('load train, validate, test data...')
+split = [17520, 4416, 4368]
+data, train_data, val_data, test_data = load_npy_data(filename=['../data/citybike/p_map.npy', '../data/citybike/d_map.npy'], split=split)
+# data: [num, row, col, channel]
+print('preprocess train data...')
+pre_process.fit(train_data)
+all_timestamps_string = gen_timestamps(['2013','2014','2015','2016'], gen_timestamps_for_year=gen_timestamps_for_year_ymdh)
+all_timestamps_string = all_timestamps_string[4344:-4416]
+all_timestamps_struct = [time.strptime(t, '%Y%m%d%H') for t in all_timestamps_string]
+timestamps = [datetime.fromtimestamp(mktime(t)) for t in all_timestamps_struct]
+# data: [num, row, col, channel]
+print('preprocess train data...')
+data = pre_process.transform(data)
+data = np.reshape(data, (data.shape[0], -1))
+
+train_data = data[:split[0]]
+val_data = data[split[0]:split[0]+split[1]]
+test_data = data[split[0]+split[1]:]
+train_timestamps = timestamps[:split[0]]
+val_timestamps = timestamps[split[0]:split[0]+split[1]]
+test_timestamps = timestamps[split[0]+split[1]:]
+
+column_name = [str(e) for e in range(1,train_data.shape[1]+1)]
+train_df = pd.DataFrame(train_data, columns=column_name)
+train_df.index = pd.DatetimeIndex(train_timestamps)
+
+#train_df = pd.DataFrame(data[:split[0]+split[1]], columns=column_name)
+#train_df.index = pd.DatetimeIndex(timestamps[:split[0]+split[1]])
+print('create VAR model and fit...')
+model_var = VAR(train_df)
+results = model_var.fit(1)
+print('test trained VAR model...')
+lag_order = results.k_ar
+val_data_preindex = np.vstack((train_data[-lag_order:], val_data))
+#val_predict = np.zeros(val_data.shape)
+test_data_preindex = np.vstack((val_data[-lag_order:], test_data))
+#test_predict = np.zeros(test_data.shape)
+# validate and test data
+val_real = []
+val_predict = []
+test_real = []
+test_predict = []
+# for i in range(val_data.shape[0]-output_steps):
+#     val_predict.append(results.forecast(val_data_preindex[i:i+lag_order], 10))
+#     val_real.append(val_data[i: i+output_steps])
+for i in range(test_data.shape[0]-output_steps):
+    test_real.append(test_data[i: i+output_steps])
+    test_predict.append(results.forecast(test_data_preindex[i:i+lag_order], 10))
+val_real = np.array(val_real)
+val_predict = np.array(val_predict)
+test_real = np.array(test_real)
+test_predict = np.array(test_predict)
+
+n_rmse_val = np.sqrt(np.sum(np.square(val_predict - val_real))*1.0/np.prod(val_real.shape))
+n_rmse_test = np.sqrt(np.sum(np.square(test_predict - test_real))*1.0/np.prod(test_real.shape))
+rmse_val = 0
+rmse_test = 0
+rmse_val = pre_process.real_loss(n_rmse_val)
+rmse_test = pre_process.real_loss(n_rmse_test)
+print('val loss is ' + str(n_rmse_val) + ' , ' + str(rmse_val))
+print('test loss is ' + str(n_rmse_test) + ' , ' + str(rmse_test))
+np.save('../citybike-results/results/VAR/test_target.npy', test_real)
+np.save('../citybike-results/results/VAR/test_prediction.npy', test_predict)
