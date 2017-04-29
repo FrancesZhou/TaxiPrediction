bac43899252dca06c6444b81516f5f610e872e8e
Frances <zhouxian@sjtu.edu.cn>
2017-04-29 11:15:14 +0800

    add taxi-10-minutes and citybike-10-minutes

---
+++ taxi-10-minutes/arma_60_days_taxi.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+from scipy import stats
+from time import mktime
+from datetime import datetime
+import pandas as pd
+
+import statsmodels.api as sm
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.vector_ar.var_model import VAR
+import sys
+# for mac debug
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/model/')
+sys.path.append('/Users/frances/Documents/DeepLearning/Code/TaxiPrediction/util/')
+# for server running
+sys.path.append('/home/zx/TaxiPrediction/model/')
+sys.path.append('../util/')
+sys.path.append('../data/')
+from utils import *
+from preprocessing import *
+
+input_steps = 10
+output_steps = 10
+run_times = 1000
+print('load train, validate, test data...')
+split = [34992, 8784, 8784]
+data, train_data, val_data, test_data = load_data(filename=['../data/taxi_10_minutes/p_map.mat', '../data/taxi_10_minutes/d_map.mat'], split=split)
+# data: [num, row, col, channel]
+print('preprocess train data...')
+all_timestamps_string = gen_timestamps(['2015'], gen_timestamps_for_year=gen_timestamps_for_year_ymdhm)
+all_timestamps_struct = [time.strptime(t, '%Y%m%d%H%M') for t in all_timestamps_string]
+timestamps = [datetime.fromtimestamp(mktime(t)) for t in all_timestamps_struct]
+# data: [num, row, col, channel]
+print('preprocess and get test data...')
+data = np.reshape(data, (data.shape[0], -1))
+#val_data = data[split[0]:split[0]+split[1]]
+test_data = data[split[0]+split[1]:]
+data = np.transpose(data)
+#val_data = np.transpose(val_data)
+test_data = np.transpose(test_data)
+print('======================= ARMA for test ===============================')
+# test first week of Nov
+# test first week of Dec
+loss = 0
+error_count = 0
+run_times = 24*7*6*2
+index_all = np.zeros(run_times)
+error_index = []
+test_target = np.zeros([run_times, output_steps])
+test_prediction = np.zeros([run_times, output_steps])
+index_all[0:1008] = range(1008)
+index_all[1008:] = range(4320,4320+1008)
+index_all = np.asarray(index_all,dtype=int)
+for r in range(run_times):
+    j = index_all[r]
+    print('run '+str(r))
+    i = np.random.randint(data.shape[0])
+    #j = np.random.randint(test_data.shape[-1]-output_steps)
+    train_df = pd.DataFrame(data[i][j:split[0]+split[1]+j])
+    train_df.index = pd.DatetimeIndex(timestamps[j:split[0]+split[1]+j])
+    try:
+        results = ARMA(train_df, order=(2,2)).fit(trend='nc', disp=-1)
+    except:
+        error_index.append(j)
+        #error_count += 1
+        continue
+    pre, _, _ = results.forecast(output_steps)
+    test_real = test_data[i][j:j+output_steps]
+    #index_all[j] = i
+    test_target[r] = test_real
+    test_prediction[r] = pre
+    #loss += np.sum(np.square(pre - test_real))
+print('================ calculate rmse for test data ============')
+#n_rmse_val = np.sqrt(np.sum(np.square(val_predict - val_real))*1.0/np.prod(val_real.shape))
+#n_rmse_test = np.sqrt(np.sum(np.square(test_predict - test_real))*1.0/np.prod(test_real.shape))
+#rmse_val = pre_process.real_loss(n_rmse_val)
+#rmse_test = pre_process.real_loss(n_rmse_test)
+#print('val loss is ' + str(n_rmse_val) + ' , ' + str(rmse_val))
+#print('test loss is ' + str(n_rmse_test) + ' , ' + str(rmse_test))
+#print('val loss is ' + str(n_rmse_val))
+error_count = len(error_index)
+error_index = np.asarray(error_index)
+loss = np.sum(np.square(test_prediction-test_target))
+print('run times: '+str(run_times))
+print('error count: '+str(error_count))
+rmse = np.sqrt(loss/((run_times-error_count)*output_steps))
+print('test loss is ' + str(rmse))
+np.save('taxi-10-minutes-results/results/ARMA/test_target.npy', test_target)
+np.save('taxi-10-minutes-results/results/ARMA/test_prediction.npy', test_prediction)
+np.save('taxi-10-minutes-results/results/ARMA/index_all.npy', index_all)
+np.save('taxi-10-minutes-results/results/ARMA/error_index.npy', error_index)
+
--- util/utils.py
+++ util/utils.py
@@ -6,52 +6,52 @@ import time
 import os
 
 def load_data(filename, split):
-	if len(filename)==2:
-		d1 = sio.loadmat(filename[0])['p_map']
-		d2 = sio.loadmat(filename[1])['d_map']
-		data = np.concatenate((d1[:,:,:,np.newaxis], d2[:,:,:,np.newaxis]), axis=3)
-	train = data[0:split[0],:,:,:]
-	validate = data[split[0]:split[0]+split[1],:,:,:]
-	test = data[split[0]+split[1]:split[0]+split[1]+split[2],:,:,:]
-	return data, train, validate, test
+    if len(filename)==2:
+        d1 = sio.loadmat(filename[0])['p_map']
+        d2 = sio.loadmat(filename[1])['d_map']
+        data = np.concatenate((d1[:,:,:,np.newaxis], d2[:,:,:,np.newaxis]), axis=3)
+    train = data[0:split[0],:,:,:]
+    validate = data[split[0]:split[0]+split[1],:,:,:]
+    test = data[split[0]+split[1]:split[0]+split[1]+split[2],:,:,:]
+    return data, train, validate, test
 
 def load_npy_data(filename, split):
-	if len(filename)==2:
-		d1 = np.load(filename[0])
-		d2 = np.load(filename[1])
-		data = np.concatenate((d1[:,:,:,np.newaxis], d2[:,:,:,np.newaxis]), axis=3)
-	train = data[0:split[0],:,:,:]
-	validate = data[split[0]:split[0]+split[1],:,:,:]
-	test = data[split[0]+split[1]:split[0]+split[1]+split[2],:,:,:]
-	return data, train, validate, test
+    if len(filename)==2:
+        d1 = np.load(filename[0])
+        d2 = np.load(filename[1])
+        data = np.concatenate((d1[:,:,:,np.newaxis], d2[:,:,:,np.newaxis]), axis=3)
+    train = data[0:split[0],:,:,:]
+    validate = data[split[0]:split[0]+split[1],:,:,:]
+    test = data[split[0]+split[1]:split[0]+split[1]+split[2],:,:,:]
+    return data, train, validate, test
 
 def load_h5data(fname):
-	f = h5py.File(fname, 'r')
-	data = f['data'].value
-	data = np.asarray(data)
-	data = np.transpose(np.asarray(data), (0,2,3,1))
-	timestamps = f['date'].value
-	f.close()
-	return data, timestamps
+    f = h5py.File(fname, 'r')
+    data = f['data'].value
+    data = np.asarray(data)
+    data = np.transpose(np.asarray(data), (0,2,3,1))
+    timestamps = f['date'].value
+    f.close()
+    return data, timestamps
 
 def batch_data(data, batch_size=32, input_steps=10, output_steps=10):
-	# data: [num, row, col, channel]
-	num = data.shape[0]
-	# x: [batches, batch_size, input_steps, row, col, channel]
-	# y: [batches, batch_size, output_steps, row, col, channel]
-	x = []
-	y = []
-	i = 0
-	while i<num-batch_size-input_steps-output_steps:
-		batch_x = []
-		batch_y = []
-		for s in range(batch_size):
-			batch_x.append(data[i+s:i+s+input_steps, :, :, :])
-			batch_y.append(data[i+s+input_steps:i+s+input_steps+output_steps, :, :, :])
-		x.append(batch_x)
-		y.append(batch_y)
-		i += batch_size
-	return x, y
+    # data: [num, row, col, channel]
+    num = data.shape[0]
+    # x: [batches, batch_size, input_steps, row, col, channel]
+    # y: [batches, batch_size, output_steps, row, col, channel]
+    x = []
+    y = []
+    i = 0
+    while i<num-batch_size-input_steps-output_steps:
+        batch_x = []
+        batch_y = []
+        for s in range(batch_size):
+            batch_x.append(data[i+s:i+s+input_steps, :, :, :])
+            batch_y.append(data[i+s+input_steps:i+s+input_steps+output_steps, :, :, :])
+        x.append(batch_x)
+        y.append(batch_y)
+        i += batch_size
+    return x, y
 # x: [batches, batch_size, 4]
 # y: [batches, batch_size, 1]
 # while i<num:
@@ -71,145 +71,174 @@ def batch_data(data, batch_size=32, input_steps=10, output_steps=10):
 # 	i += batch_size
 
 def batch_data_cpt_ext(data, timestamps, batch_size=32, close=3, period=4, trend=4):
-	# data: [num, row, col, channel]
-	num = data.shape[0]
-	#flow = data.shape[1]
-	# x: [batches, 
-	#[
-	#[batch_size, row, col, close*flow], 
-	#[batch_size, row, col, period*flow], 
-	#[batch_size, row, col, trend*flow],
-	#[batch_size, external_dim]
-	#]
-	#]
-	c = 1
-	p = 24
-	t = 24*7
-	depends = [ [c*j for j in range(1, close+1)],
-				[p*j for j in range(1, period+1)],
-				[t*j for j in range(1, trend+1)] ]
-	depends = np.asarray(depends)
-	i = max(c*close, p*period, t*trend)
-	# external feature
-	ext = external_feature(timestamps)
-	# ext plus c p t
-	# x: [batches, 4, batch_size]
-	# y: [batches, batch_size]
-	x = []
-	y = []
-	while i<num:
-		x_b = np.empty(len(depends)+1, dtype=object)
-		for d in range(len(depends)):
-			x_ = []
-			for b in range(batch_size):
-				if i+b >= num:
-					break
-				x_.append(np.transpose(np.vstack(np.transpose(data[i+b-np.array(depends[d]), :, :, :],[0,3,1,2])), [1,2,0]))
-			x_ = np.array(x_)
-			x_b[d] = x_
-			#x_b.append(x_)
-		# external features
-		x_b[-1] = ext[i:min(i+batch_size, num)]
-		# y
-		y_b = data[i:min(i+batch_size, num), :, :, :]
-		x.append(x_b)
-		#print(y_b.shape)
-		y.append(y_b)
-		i += batch_size
-	return x, y
+    # data: [num, row, col, channel]
+    num = data.shape[0]
+    #flow = data.shape[1]
+    # x: [batches,
+    #[
+    #[batch_size, row, col, close*flow],
+    #[batch_size, row, col, period*flow],
+    #[batch_size, row, col, trend*flow],
+    #[batch_size, external_dim]
+    #]
+    #]
+    c = 1
+    p = 24
+    t = 24*7
+    depends = [ [c*j for j in range(1, close+1)],
+                [p*j for j in range(1, period+1)],
+                [t*j for j in range(1, trend+1)] ]
+    depends = np.asarray(depends)
+    i = max(c*close, p*period, t*trend)
+    # external feature
+    ext = external_feature(timestamps)
+    # ext plus c p t
+    # x: [batches, 4, batch_size]
+    # y: [batches, batch_size]
+    x = []
+    y = []
+    while i<num:
+        x_b = np.empty(len(depends)+1, dtype=object)
+        for d in range(len(depends)):
+            x_ = []
+            for b in range(batch_size):
+                if i+b >= num:
+                    break
+                x_.append(np.transpose(np.vstack(np.transpose(data[i+b-np.array(depends[d]), :, :, :],[0,3,1,2])), [1,2,0]))
+            x_ = np.array(x_)
+            x_b[d] = x_
+            #x_b.append(x_)
+        # external features
+        x_b[-1] = ext[i:min(i+batch_size, num)]
+        # y
+        y_b = data[i:min(i+batch_size, num), :, :, :]
+        x.append(x_b)
+        #print(y_b.shape)
+        y.append(y_b)
+        i += batch_size
+    return x, y
 
 def external_feature(timestamps):
-	vec = [time.strptime(t[:8], '%Y%m%d').tm_wday for t in timestamps]
-	ext = []
-	for j in vec:
-		v = [0 for _ in range(7)]
-		v[j] = 1
-		if j >= 5:
-			v.append(0)  # weekend
-		else:
-			v.append(1)  # weekday
-		ext.append(v)
-	ext = np.asarray(ext)
-	return ext
+    vec = [time.strptime(t[:8], '%Y%m%d').tm_wday for t in timestamps]
+    ext = []
+    for j in vec:
+        v = [0 for _ in range(7)]
+        v[j] = 1
+        if j >= 5:
+            v.append(0)  # weekend
+        else:
+            v.append(1)  # weekday
+        ext.append(v)
+    ext = np.asarray(ext)
+    return ext
 
 def gen_timestamps_for_year(year):
-	month1 = ['0'+str(e) for e in range(1,10)]
-	month2 = [str(e) for e in range(10,13)]
-	month = month1+month2
-	day1 = ['0'+str(e) for e in range(1,10)]
-	day2 = [str(e) for e in range(10,32)]
-	day = day1+day2
-	if year=='2012' or year=='2016':
-		day_sum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-	else:
-		day_sum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-	timestamps = []
-	for m in range(len(month)):
-		for d in range(day_sum[m]):
-			t = [year+month[m]+day[d]]
-			t_d = t*24
-			timestamps.append(t_d[:])
- 	timestamps = np.hstack(np.array(timestamps))
- 	return timestamps
+    month1 = ['0'+str(e) for e in range(1,10)]
+    month2 = [str(e) for e in range(10,13)]
+    month = month1+month2
+    day1 = ['0'+str(e) for e in range(1,10)]
+    day2 = [str(e) for e in range(10,32)]
+    day = day1+day2
+    if year=='2012' or year=='2016':
+        day_sum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else:
+        day_sum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    timestamps = []
+    for m in range(len(month)):
+        for d in range(day_sum[m]):
+            t = [year+month[m]+day[d]]
+            t_d = t*24
+            timestamps.append(t_d[:])
+    timestamps = np.hstack(np.array(timestamps))
+    return timestamps
 
 def gen_timestamps(years, gen_timestamps_for_year=gen_timestamps_for_year):
-	timestamps = []
-	for y in years:
-		timestamps.append(gen_timestamps_for_year(y))
-	timestamps = np.hstack(np.array(timestamps))
-	return timestamps
+    timestamps = []
+    for y in years:
+        timestamps.append(gen_timestamps_for_year(y))
+    timestamps = np.hstack(np.array(timestamps))
+    return timestamps
 
 def gen_timestamps_for_year_ymdh(year):
-	month1 = ['0'+str(e) for e in range(1,10)]
-	month2 = [str(e) for e in range(10,13)]
-	month = month1+month2
-	day1 = ['0'+str(e) for e in range(1,10)]
-	day2 = [str(e) for e in range(10,32)]
-	day = day1+day2
-	if year=='2012' or year=='2016':
-		day_sum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-	else:
-		day_sum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-	hour1 = ['0'+str(e) for e in range(0,10)]
-	hour2 = [str(e) for e in range(10,24)]
-	hour = hour1+hour2
-	timestamps = []
-	for m in range(len(month)):
-		for d in range(day_sum[m]):
-			#t = [year+month[m]+day[d]]
-			t_d = []
-			for h in range(24):
-				t_d.append(year+month[m]+day[d]+hour[h])
-			timestamps.append(t_d[:])
- 	timestamps = np.hstack(np.array(timestamps))
- 	return timestamps
+    month1 = ['0'+str(e) for e in range(1,10)]
+    month2 = [str(e) for e in range(10,13)]
+    month = month1+month2
+    day1 = ['0'+str(e) for e in range(1,10)]
+    day2 = [str(e) for e in range(10,32)]
+    day = day1+day2
+    if year=='2012' or year=='2016':
+        day_sum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else:
+        day_sum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    hour1 = ['0'+str(e) for e in range(0,10)]
+    hour2 = [str(e) for e in range(10,24)]
+    hour = hour1+hour2
+    timestamps = []
+    for m in range(len(month)):
+        for d in range(day_sum[m]):
+            #t = [year+month[m]+day[d]]
+            t_d = []
+            for h in range(24):
+                t_d.append(year+month[m]+day[d]+hour[h])
+            timestamps.append(t_d[:])
+    timestamps = np.hstack(np.array(timestamps))
+    return timestamps
+
+def gen_timestamps_for_year_ymdhm(year):
+    month1 = ['0'+str(e) for e in range(1,10)]
+    month2 = [str(e) for e in range(10,13)]
+    month = month1+month2
+    day1 = ['0'+str(e) for e in range(1,10)]
+    day2 = [str(e) for e in range(10,32)]
+    day = day1+day2
+    if year=='2012' or year=='2016':
+        day_sum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else:
+        day_sum = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    hour1 = ['0'+str(e) for e in range(0,10)]
+    hour2 = [str(e) for e in range(10,24)]
+    hour = hour1+hour2
+    minute = ['00', '10', '20', '30', '40', '50']
+    timestamps = []
+    for m in range(len(month)):
+        for d in range(day_sum[m]):
+            #t = [year+month[m]+day[d]]
+            t_d = []
+            for h in range(24):
+                a = [year+month[m]+day[d]+hour[h]+e for e in minute]
+                #t_d = [t_d.append(year+month[m]+day[d]+hour[h]+e) for e in minute]
+                t_d.append(a)
+            t_d = np.hstack(np.array(t_d))
+            timestamps.append(t_d[:])
+    timestamps = np.hstack(np.array(timestamps))
+    return timestamps
 
 def shuffle_batch_data(data, batch_size=32, input_steps=10, output_steps=10):
-	num = data.shape[0]
-	# shuffle
-	data = data[np.random.shuffle(np.arange(num)), :, :, :]
+    num = data.shape[0]
+    # shuffle
+    data = data[np.random.shuffle(np.arange(num)), :, :, :]
 
-	x = []
-	y = []
-	i = 0
-	while i<num-batch_size-input_steps-output_steps:
-		batch_x = []
-		batch_y = []
-		for s in range(batch_size):
-			batch_x.append(data[i+s:i+s+input_steps, :, :, :])
-			batch_y.append(data[i+s+input_steps:i+s+input_steps+output_steps, :, :, :])
-		x.append(batch_x)
-		y.append(batch_y)
-		i += batch_size
-	return x, y
+    x = []
+    y = []
+    i = 0
+    while i<num-batch_size-input_steps-output_steps:
+        batch_x = []
+        batch_y = []
+        for s in range(batch_size):
+            batch_x.append(data[i+s:i+s+input_steps, :, :, :])
+            batch_y.append(data[i+s+input_steps:i+s+input_steps+output_steps, :, :, :])
+        x.append(batch_x)
+        y.append(batch_y)
+        i += batch_size
+    return x, y
 
 def load_pickle(path):
-	with open(path, 'rb') as f:
-		file = pickle.load(f)
-		print('Loaded %s..' %path)
-		return file
+    with open(path, 'rb') as f:
+        file = pickle.load(f)
+        print('Loaded %s..' %path)
+        return file
 
-def save_pickle(path):
-	with open(path, 'rb') as f:
-		pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-		print('Saved %s..' %path)
+def save_pickle(path,data):
+    with open(path, 'rb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        print('Saved %s..' %path)
